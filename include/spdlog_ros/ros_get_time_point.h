932ddd59c2d88412389d1fac1cc8a008e36beed6
Clemens Mühlbacher <c.muehlbacher@arti-robots.com>
2025-09-15 09:54:04 +0200

    fixed issue of missing get time function

--- include/spdlog_ros/ros_get_time_point.h
+++ include/spdlog_ros/ros_get_time_point.h
@@ -5,4 +5,20 @@
 #ifndef SPDLOG_ROS_ROS_GET_TIME_POINT_H
 #define SPDLOG_ROS_ROS_GET_TIME_POINT_H
 
+#include <ros/time.h>
+
+typedef uint64_t spdlog_ros_utils_time_point_value_t;
+typedef uint64_t spdlog_ros_utils_duration_value_t;
+typedef uint64_t spdlog_ros_utils_ret_t;
+
+#define GET_TIME_POINT(clock) \
+    auto get_time_point = [](spdlog_ros_utils_time_point_value_t * time_point) -> spdlog_ros_utils_ret_t { \
+      try { \
+        *time_point = ros::Time::now().toNSec(); \
+      } catch (...) { \
+        return SPDLOG_ROS_UTILS_RET_ERROR; \
+      } \
+        return SPDLOG_ROS_UTILS_RET_OK; \
+    }; \
+
 #endif //SPDLOG_ROS_ROS_GET_TIME_POINT_H
